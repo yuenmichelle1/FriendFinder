59bc1b3f8d492dae1f532523b42941163f6c6f85
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-02-27 17:02:30 -0600

    touching more stuff

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+// array of objects /friends
+[{
+    "name":  "Michelle", 
+    "photo": ,
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+    ]
+}]
