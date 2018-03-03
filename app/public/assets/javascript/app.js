7a1dc0817a25d432eb8f9dfe09cf3def4520d16a
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-03-02 18:21:57 -0600

    kind of got the logic still needs work

--- app/public/assets/javascript/app.js
+++ app/public/assets/javascript/app.js
@@ -1,2 +1,42 @@
-var friends = require("../app/data/friends");
-console.log(friends);
+var score= [];
+var userData;
+function survey(){
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+    
+        // grab from user data
+        grabScore();
+        console.log(userData);
+    }) 
+}
+
+function grabScore(){
+    if ($("select").val === "" || $("input").val() == "" ) {
+        alert('Need to answer all questions');
+    } else{
+        $("select").each(function(){
+            if($(this).val() != ""){
+                score .push($(this).val());
+
+            } 
+        })
+        defineNewUser();
+    }
+}
+
+function defineNewUser() {
+    userData = {
+        name: $("#name").val(),
+        score: score
+    }
+    postUserData();
+}
+
+function postUserData(){
+    // .post(url, data, callback)
+    $.post("/api/friends", userData, function(data){
+        console.log(data);
+    })
+}
+
+survey();
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,10 @@
 // post route to /api/friendse
 var friends =  require("../data/friends");
 
-// console.log(friends);
-var bunny;
-
 function friendsRoutes(app) {
     app.get("/api/friends", (req, res) => res.json(friends));
+    app.post("/api/friends", (req, res) => friends.push(req.body));
+    // do math in post route 
 }
 
 
